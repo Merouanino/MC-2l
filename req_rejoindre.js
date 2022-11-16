ece7f4e3bd25039f6b82a5d010d75ab5c5e600e2
Merouane <aitmokhtar@et.intechinfo.fr>
2022-11-16 10:17:08 +0100

    Req_rejoindre avec la liste de marqueurs

--- req_rejoindre.js
+++ req_rejoindre.js
@@ -4,36 +4,41 @@ const fs = require("fs");
 const nj = require("nunjucks");
 
 const req_rejoindre = function (req,res,query){
-	let page = fs.readFileSync(`lobby.html`, "UTF-8");	
+	let page = fs.readFileSync(`modele_lobby.html`, "UTF-8");	
 	let contenu;
+	
 	//Récupération du Contexte
+	
 	let choix = query.choix;
 	let pseudo = query.pseudo;
 	let lobby = fs.readFileSync("lobbys.json", "UTF-8");
-	lobby = JSON.parse(contenu);
+	lobby = JSON.parse(lobby);
 	let membres = fs.readFileSync("membres.json", "UTF-8");
 	membres = JSON.parse(membres);
+	
 	//Traitement
+	
 	for (let i = 0; i < membres.length; i++){
 		if (membres[i].pseudo === pseudo){
 			lobby[choix].joueurs.push(i);
 		}
 	}
 	
-	pseudos = [];
+	let pseudos = [];
 
 	for (let j = 0; j < lobby[choix].joueurs.length; j++){
-		pseudos.push(membres[ lobby.joueurs[i] ]);
+		pseudos.push(membres[ lobby[j].joueurs ]);
 	}
 	
 	//Mémorisation du Contexte
 
 	contenu = JSON.stringify(lobby);
-	fs.writeFileSync("lobby.json", contenu, "utf-8");
+	fs.writeFileSync("lobbys.json", contenu, "utf-8");
 
 	//Fabrication et envoi de la page HTML
 
 	let marqueurs = {};
+	marqueurs["pseudos"] = pseudos;	
 	marqueurs.erreur = "";
 	marqueurs.pseudo = "";
 	page = nj.renderString(page,marqueurs);
