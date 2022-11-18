bf13ec3024d13fdaf1c2fbb47660d07858fb5113
Cerisesss <cding847@gmail.com>
2022-11-18 13:25:43 +0100

    remise au propre des codes

--- req_quitter_lobby.js
+++ req_quitter_lobby.js
@@ -15,17 +15,20 @@ const req_quitter_lobby = function (req, res, query) {
 	let choix;
 
 	//Récupération des crédits associés au compte
+
 	let coins;
 	let members;
+	
 	pseudo = query.pseudo;
+	
 	members = fs.readFileSync("membres.json", "UTF-8");
 	members = JSON.parse(members);
+	
 	for (let i = 0; i < members.length; i++){
 		if(members[i].pseudo === pseudo){
 			coins = members[i].coins;
 		}
 	}
-
 
 	requete = url.parse(req.url, true);
     pathname = requete.pathname;
@@ -41,26 +44,23 @@ const req_quitter_lobby = function (req, res, query) {
 	choix = query.choix;
 	joueur = lobby[choix].joueurs.indexOf(joueur);
 	
-	//joueur = lobby.joueurs.indexOf(joueur);
-	//choix = lobby.choix.indexOf(choix);
-	
 	//supprétion du joueur ayant quitter le lobby
 	
-	lobby[choix].joueurs.splice(joueur, 1); // faut recup la bonne liste -> bon choix (table)
-	//lobby.splice(joueur, 1); // supp ce que contient json sniff ;-;
+	lobby[choix].joueurs.splice(joueur, 1);
+	
 	lobby = JSON.stringify(lobby);
 	lobby = fs.writeFileSync("lobbys.json", lobby, 'utf-8');
-
-	//page = nunjucks.renderString(page, marqueurs);
 	
 	page = fs.readFileSync(`modele_accueil_membre.html`, "UTF-8");
+	
 	marqueurs = {};
-	//marqueurs["pseudos"] = pseudos;
 	marqueurs.pseudo = pseudo;
 	marqueurs.choix = choix;
 	marqueurs.credits = coins;
+	
 	page = nj.renderString(page,marqueurs);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    
+	res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(page);
     res.end();
 };
--- req_rejoindre.js
+++ req_rejoindre.js
@@ -9,14 +9,20 @@ const req_rejoindre = function (req,res,query){
 	let contenu;
 	let requete;
 	let pathname;
+	let choix;
+	let pseudo;
+	let lobby;
+	let membres;
 	
 	//Récupération du Contexte
 	
-	let choix = query.choix;
-	let pseudo = query.pseudo;
-	let lobby = fs.readFileSync("lobbys.json", "UTF-8");
+	choix = query.choix;
+	pseudo = query.pseudo;
+	
+	lobby = fs.readFileSync("lobbys.json", "UTF-8");
 	lobby = JSON.parse(lobby);
-	let membres = fs.readFileSync("membres.json", "UTF-8");
+	
+	membres = fs.readFileSync("membres.json", "UTF-8");
 	membres = JSON.parse(membres);
 
 	requete = url.parse(req.url, true);
@@ -46,11 +52,11 @@ const req_rejoindre = function (req,res,query){
 
 	let marqueurs = {};
 	marqueurs["pseudos"] = pseudos;	
-	//marqueurs.erreur = "";
 	marqueurs.pseudo = pseudo;
 	marqueurs.choix = choix;
 
 	page = nj.renderString(page,marqueurs);
+	
 	res.writeHead(200,{ 'Content-Type' : 'text/html' });
 	res.write(page);
 	res.end();
