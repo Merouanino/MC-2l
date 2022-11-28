50dbd9df34b834ac9ac94d3a5b46b603c7baef70
Merouane <aitmokhtar@et.intechinfo.fr>
2022-11-28 14:18:23 +0100

    Ajout du marqueur actif

--- req_rafraichir.js
+++ req_rafraichir.js
@@ -21,9 +21,8 @@ const req_rafraichir = function (req,res,query){
 	let gains;
 	let mise;
 	let blackjack;
-	const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51];
-	
-	cartes.sort(()=> Math.random()-0.5);
+	let actif;
+
 	//Récupération du Contexte
 
 	requete = url.parse(req.url, true);
@@ -62,7 +61,7 @@ const req_rafraichir = function (req,res,query){
 	marqueurs.pseudo = pseudo;
 	marqueurs.choix = choix;
 	marqueurs.table = table;
-
+	marqueurs.actif = tables[choix].joueur[tables[choix].actif] === query.pseudo;
 	page = nj.renderString(page,marqueurs);
 	
 	res.writeHead(200,{ 'Content-Type' : 'text/html' });
