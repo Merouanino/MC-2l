e719aa08b07b02e2f692218a56497e98c96380f7
Merouane <aitmokhtar@et.intechinfo.fr>
2022-11-28 14:23:34 +0100

    Sort of corrigé

--- fct_initialisation.js
+++ fct_initialisation.js
@@ -36,7 +36,7 @@ const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
     c = c.concat(cartes);
     c = c.concat(cartes);
 
-    cartes.sort(()=> Math.random()-0.5);
+    c.sort(()=> Math.random()-0.5);
 	
 	};
 module.exports = {
