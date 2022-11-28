2ea2ad9d66fff6a57e2a56c2de43a668aa3567b0
Cerisesss <cding847@gmail.com>
2022-11-28 14:00:45 +0100

    supprétion des joueurs ayant quitter le jeu

--- req_quitter.js
+++ req_quitter.js
@@ -9,44 +9,33 @@ const req_quitter_lobby = function (req, res, query) {
 	let pathname;
     let marqueurs;
 	let page;
-    let joueur;
 	let tables;
 	let pseudo;
 	let choix;
-
-	//Récupération des crédits associés au compte
-
-	let coins;
 	let members;
-	
-	pseudo = query.pseudo;
-	
-	members = fs.readFileSync("membres.json", "UTF-8");
-	members = JSON.parse(members);
-	
-	for (let i = 0; i < members.length; i++){
-		if(members[i].pseudo === pseudo){
-			coins = members[i].coins;
-		}
-	}
+
 
 	requete = url.parse(req.url, true);
     pathname = requete.pathname;
     query = requete.query;
 
 	//Lecture du fichier json 
-	
+		
+	members = fs.readFileSync("membres.json", "UTF-8");
+	members = JSON.parse(members);
+
 	tables = fs.readFileSync("tables.json", "UTF-8");
     tables = JSON.parse(tables);
 
 	//récupération du pseudo depuis url et trouve l'indice du joueur dans le tableau joueur
 	
+	pseudo = query.pseudo;
 	choix = query.choix;
-	joueur = lobby[choix].joueurs.indexOf(joueur);
+	pseudo = tables[choix].joueurs.indexOf(pseudo);
 	
-	//supprétion du joueur ayant quitter la page
+	//supprétion du joueur ayant quitter la table
 	
-	lobby[choix].joueurs.splice(joueur, 1);
+	tables[choix].joueurs.splice(joueur, 1);
 	
 	tables = JSON.stringify(tables);
 	tables = fs.writeFileSync("tables.json", tables, 'utf-8');
@@ -56,7 +45,6 @@ const req_quitter_lobby = function (req, res, query) {
 	marqueurs = {};
 	marqueurs.pseudo = pseudo;
 	marqueurs.choix = choix;
-	marqueurs.credits = coins;
 	
 	page = nj.renderString(page,marqueurs);
     
