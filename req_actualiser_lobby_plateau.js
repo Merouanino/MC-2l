d2a9146779e6e2cbde551645cde37ec0f32f600d
Merouane <aitmokhtar@et.intechinfo.fr>
2022-11-28 10:36:46 +0100

    Initialisation des cartes à peine commencée

--- req_actualiser_lobby_plateau.js
+++ req_actualiser_lobby_plateau.js
@@ -37,6 +37,35 @@ const actualiser_lobby_plateau = function (req, res, query) {
 	choix = query.choix;
 	pseudo = query.pseudo;
 
+    //Initialisation du paquet de cartes 
+    const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51];
+    function carte(c){
+		return Math.floor(Math.random() * 52);
+	};
+
+	function couleur(c){
+		return 
+	};
+    
+	function valeur(c){
+		let v;
+		if(c >= 10){
+			v = 10;
+		}
+		else{
+			v = c;
+		}
+		return v;
+	};
+
+	cartes = cartes.concat(cartes);
+    cartes = cartes.concat(cartes);
+    cartes = cartes.concat(cartes);
+    cartes = cartes.concat(cartes);
+    cartes = cartes.concat(cartes);
+
+    cartes.sort(()=> Math.random()-0.5);
+
 	//Vérifie que les joueurs sont là et récupère leur mise, si ts les joueurs du tab sont là on les envoit vers le plateau
 
 	joueur = tables[choix].joueurs;
