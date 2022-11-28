96687eaaf7c3e402a67c5d8f126f86e464c2b9d5
Léa Pedra <pedra@et.intechinfo.fr>
2022-11-28 14:10:34 +0100

    modif de la condition pour passer à la page d'après

--- req_actualiser_lobby_plateau.js
+++ req_actualiser_lobby_plateau.js
@@ -37,6 +37,29 @@ const actualiser_lobby_plateau = function (req, res, query) {
 	choix = query.choix;
 	pseudo = query.pseudo;
 
+	//Vérifie que les joueurs sont là et récupère leur mise, si ts les joueurs du tab sont là on les envoit vers le plateau
+
+	//joueur = tables[choix].joueurs;
+	//joueur_mise = tables[choix].mises;
+	joueur = tables[choix].joueurs.filter(el1 => el1 === null);
+	joueur_mise = tables[choix].mises.filter(el2 => el2 === null);
+	
+	//rajouter mise dans json
+	if(joueur.length === joueur_mise.length){
+		page = fs.readFileSync("modele_plateau.html", "utf-8");
+	}else{
+		page = fs.readFileSync("modele_lobby_plateau.html", "utf-8");
+	}
+
+	//on ne peut plus rejoindre la table car etat = false
+
+	tables[choix].etat = false;
+
+	//on enregistre
+	
+	contenu = fs.readFileSync("tables.json", "UTF-8");
+    
+	tables = JSON.parse(contenu);
     //Initialisation du paquet de cartes 
     const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51];
     function carte(c){
@@ -65,27 +88,6 @@ const actualiser_lobby_plateau = function (req, res, query) {
     cartes = cartes.concat(cartes);
 
     cartes.sort(()=> Math.random()-0.5);
-
-	//Vérifie que les joueurs sont là et récupère leur mise, si ts les joueurs du tab sont là on les envoit vers le plateau
-
-	joueur = tables[choix].joueurs;
-	joueur_mise = tables[choix].mises;
-	
-	//rajouter mise dans json
-	if(joueur.length === joueur_mise.length){
-		page = fs.readFileSync("modele_plateau.html", "utf-8");
-	}else{
-		page = fs.readFileSync("modele_lobby_plateau.html", "utf-8");
-	}
-
-	//on ne peut plus rejoindre la table car etat = false
-
-	tables[choix].etat = false;
-
-	//on enregistre
-	
-	contenu = fs.readFileSync("tables.json", "UTF-8");
-    tables = JSON.parse(contenu);
 
 	marqueurs = {};
 	marqueurs.pseudo = pseudo;
