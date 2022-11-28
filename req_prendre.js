116c4a605f7d7bf54342eac042cb122d40864195
Lucien Cabanel <cabanel@et.intechinfo.fr>
2022-11-28 17:19:40 +0100

    j'ai enlever la derniere ligne de code useless

--- req_prendre.js
+++ req_prendre.js
@@ -1,4 +1,5 @@
 "use strict"
+const { table } = require("console");
 const fs = require("fs");
 const nj = require("nunjucks");
 const url = require("url");
@@ -9,18 +10,46 @@ const req_prendre = function (req,res,query){
 	let requete;
 	let pathname;
 	let pseudo;
-    let lobby;
-	let membres;
-    let cartes;
-
-
+    let carte;
+    let choix;
+    let marqueurs;
     //Récupération du Contexte
     requete = url.parse(req.url, true);
 	pathname = requete.pathname;
 	query = requete.query
 
     pseudo = query.pseudo;
+    choix = query.choix;
+
+    tables = fs.readFileSync("tables.json", "UTF-8");
+	tables = JSON.parse(tables);
+    page = fs.readFileSync("modele_plateau.html", "utf-8")
+
+    //Traitement
+
+    carte_random = Math.floor(Math.random() * 52)+1;
+    tables[choix].cartes.splice(carte_random, 1);
+
+    if(tables[choix].cartes.length < 3*52){
+
+    }
     
-    lobby = fs.readFileSync("lobbys.json", "UTF-8");
-	lobby = JSON.parse(lobby);
-}
+
+    //Mémorisation du Contexte
+    contenu = JSON.stringify("table.json");
+    fs.writeFileSync("tables.json", contenu,"utf-8")
+
+
+    //Fabrication et envoi de la page HTML
+    marqueurs = {};
+    marqueurs.carte = carte_random;
+
+    page = fs.readFileSync(`modele_plateau.html`, "UTF-8");
+	page = nj.renderString(page,marqueurs);
+
+    res.writeHead(200, { 'Content-Type' : 'text/html'});
+	res.write(page);
+	res.end();
+  
+};
+module.exports = req_prendre;
