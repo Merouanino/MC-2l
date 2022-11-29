e27fd8e9fd672f1e0ac74471e9ec38a3d01e3189
Merouane <aitmokhtar@et.intechinfo.fr>
2022-11-29 14:26:19 +0100

    Ajout de la liste actif dans tables.json

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ try{
 } catch (err) {
 	if (err.code === "ENOENT"){
 		fs.writeFileSync("tables.json", JSON.stringify([
-			{"joueurs" : [],"etat" : true, "mises" : [], "cartes" : [], "main" : [[], [], [], [], []]},
-			{"joueurs" : [],"etat" : true, "mises" : [], "cartes" : [], "main" : [[], [], [], [], []]},
-			{"joueurs" : [],"etat" : true, "mises" : [], "cartes" : [], "main" : [[], [], [], [], []]}
+			{"joueurs" : [],"etat" : true, "mises" : [], "cartes" : [], "main" : [[], [], [], [], []], "actif" : []},
+			{"joueurs" : [],"etat" : true, "mises" : [], "cartes" : [], "main" : [[], [], [], [], []], "actif" : []},
+			{"joueurs" : [],"etat" : true, "mises" : [], "cartes" : [], "main" : [[], [], [], [], []], "actif" : []}
 		]), "UTF-8");
 	}
 }
