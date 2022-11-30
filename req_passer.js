ccae4c85cf08d11db433b03be84d9f6f576bbb2e
Lucien Cabanel <cabanel@et.intechinfo.fr>
2022-11-30 13:13:33 +0100

    premier test de req_passer

---
+++ req_passer.js
@@ -0,0 +1,58 @@
+"use strict";
+const fs = require("fs");
+const nj = require("nunjucks");
+const url = require("url");
+const fct = require("./fct_initialisation.js");
+
+const req_passer = function (req,res,query){
+	let page;
+	let contenu;
+	let requete;
+	let pathname;
+	let pseudo;
+	let choix;
+	let marqueurs = {};
+	let tables;
+
+	//Récupération du Contexte
+
+	requete = url.parse(req.url, true);
+	pathname = requete.pathname;
+	query = requete.query;
+
+	pseudo = query.pseudo;
+	choix = query.choix;
+
+	tables = fs.readFileSync("tables.json", "UTF-8");
+	tables = JSON.parse(tables);
+	
+	page = fs.readFileSync("modele_plateau.html", "utf-8");
+
+	//Traitement
+	for(let i = 0 ; i > tables[choix].joueurs.length; i++ ){
+		tables[choix].actif += 1;
+	}
+	
+	
+	//Mémorisation du Contexte
+
+	contenu = JSON.stringify(tables);
+	fs.writeFileSync("tables.json", contenu, "utf-8");
+
+	//Fabrication et envoi de la page HTML
+	marqueurs = {};
+	marqueurs.pseudo = pseudo;
+	marqueurs.choix = choix;
+	marqueurs.actif = tables[choix].actif;
+
+	page = fs.readFileSync(`modele_plateau.html`, "UTF-8");
+	page = nj.renderString(page,marqueurs);
+
+	res.writeHead(200, { 'Content-Type' : 'text/html'});
+	res.write(page);
+	res.end();
+
+};  
+module.exports = req_prendre;
+
+
