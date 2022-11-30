1fa7edd52518756614d8f59637994a7098ba0a3a
Merouane <aitmokhtar@et.intechinfo.fr>
2022-11-30 15:33:30 +0100

    Miaou

--- req_rafraichir.js
+++ req_rafraichir.js
@@ -24,6 +24,9 @@ const req_rafraichir = function (req,res,query){
 	let blackjack;
 	let actif;
 	let marqueurs = {};
+	let indice;
+	let indice_pseudo;
+	let gain;
 
 	//Récupération du Contexte
 
@@ -44,16 +47,17 @@ const req_rafraichir = function (req,res,query){
 	choix = query.choix;
 	pseudo = query.pseudo;
 	
-
 	//Traitement
 	
 	//Calcul des mains
 	
 	for(let m = 0; m < tables[choix].banque.length; m++){
-		croupier += Number(fct.valeur(tables[choix].banque[m]));
-		console.log(croupier);
+		for(let o = 0; o < tables[choix].banque[m].length; o++){
+			croupier += Number(fct.valeur(tables[choix].banque[m][o]));
+			console.log(croupier);
+		}
 	}
-	
+		
 	for(let i = 0; i < tables[choix].joueurs.length; i++){
 		if(tables[choix].joueurs[i] !== null){
 			for(let j = 0; j < tables[choix].main[i].length; j++){
@@ -72,11 +76,50 @@ const req_rafraichir = function (req,res,query){
 		marqueurs.mains = tables[choix].main[l];
 	}
 	
+	//Les cartes de la banque
+
+	if (croupier < 17 && tables[choix].actif == tables[choix].joueurs.length ){
+		tables[choix].banque.push(fct.valeur());
+	}
+
+	//Les conditions de victoire
+	
+	//recup l'indice du joueur dans membres.json
+	for(let n = 0; n < membres.length; i++){
+			if(membres[n].pseudo === pseudo){
+				indice = n;
+			}
+		}
+	
+	//recup l'indice du joueur qui se trouve dans tables.json (meme indice que pour la mise)
+	indice_pseudo = tables[choix].joueurs.indexOf(indice);
+
+	//recup la valeur de la mise du joueur 
+	gain = tables[choix].mises[indice_pseudo];
+
+
+	//verification
+	if(somme === 21){
+		if(croupier === 21){
+			membres[indice].coins += gain;
+		}else{
+			membres[indice].coins += gain * 2;
+		}
+	}else if(somme < 21){
+		if(croupier > 21 || (croupier < 21 && croupier < somme)){
+			membres[indice].coins += gain * 2;
+		}else if(croupier === somme){
+			membres[indice].coins += gain;
+		}
+
 	//Mémorisation du Contexte
 
 	tables = JSON.stringify(tables, null, "\t");
 	fs.writeFileSync("tables.json", tables, "utf-8");
 
+	membres = JSON.stringify(membres, null, "\t");
+	fs.writeFileSync("membres.json", membres, "UTF-8");
+	
 	//Fabrication et envoi de la page HTML
 
 	marqueurs.pseudo = pseudo;
