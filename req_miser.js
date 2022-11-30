4eb4889d1ed937cee86f9067348b9bb6f4c192aa
Cerisesss <cding847@gmail.com>
2022-11-30 12:17:20 +0100

    V10000

--- req_miser.js
+++ req_miser.js
@@ -16,9 +16,11 @@ const req_miser = function (req,res,query){
 	let mise;
 	let coins;
 	let choix;
+
 	let joueur;
-	let continuer;
 	let indice_pseudo;
+	let i;
+	let indice;
 
 	//Récupération du Contexte
 	
@@ -39,34 +41,40 @@ const req_miser = function (req,res,query){
 	tables = fs.readFileSync("tables.json", "UTF-8");
 	tables = JSON.parse(tables);
 
-	//Recup l'indice du joueur dans la liste des joueurs du fichier tables.json
+	//recup l'indice du joueur dans membres.json
+	for(i = 0; i < membres.length ; i++){
+		if(membres[i].pseudo === pseudo){
+			indice = i;
+		}
+	}
 
-	indice_pseudo = tables[choix].joueurs.indexOf(pseudo);
+	//Recup l'indice du joueur dans la liste des joueurs du fichier tables.json
+	indice_pseudo = tables[choix].joueurs.indexOf(indice);
 
 	//initialisation de la liste des mises
-	for(let i = 0; i < 4; i++){
+	while(tables[choix].mises.length < 5){
 		tables[choix].mises.push(null);
 	}
 
 
 	//Traitement
 	
-	for (let i = 0; i < membres.length; i++){
-		if (membres[i].pseudo === pseudo && membres[i].coins > mise && membres[i].coins - mise > 0){
-			membres[i].coins -= mise;
+	for (let j = 0; j < membres.length; j++){
+		if (membres[j].pseudo === pseudo && membres[j].coins > mise && membres[j].coins - mise > 0){
+			membres[j].coins -= mise;
 			
 		}
 	}
 	
 	//ajoute la mise des joueurs 
-	
+	console.log(indice_pseudo);
+	console.log(indice);
 	for (let j = 0; j < membres.length; j++){
 		if (membres[j].pseudo === pseudo && tables[choix].etat === true){
-			tables[choix].mises.splice(indice_pseudo, 1, mise);
-			//tables[choix].mises.push(mise);
+			//tables[choix].mises.splice(indice_pseudo, 1, mise);
+			tables[choix].mises[indice_pseudo] = mise;
 		}
 	}
-
 
 	//Mémorisation du Contexte
 	
