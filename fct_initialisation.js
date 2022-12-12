923c714b369409394727812231b6a58fba8256aa
Merouane <aitmokhtar@et.intechinfo.fr>
2022-12-12 15:05:18 +0100

    Indentation plus propre

--- fct_initialisation.js
+++ fct_initialisation.js
@@ -3,7 +3,8 @@
 const fs = require("fs");
 
 const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51];
-    function carte(cartes){
+    
+	function carte(cartes){
         const carte_index = Math.floor(Math.random() * cartes.length);
 		const carte = cartes[carte_index];
 		cartes.splice(carte_index, 1);
@@ -65,7 +66,7 @@ const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
 				console.log("total 10 :" + total);
 			}
 		return total;
-	}
+	};
 	
 	function liste_attente(l1,l2){
 		let ls = [];
@@ -73,7 +74,7 @@ const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
 			ls.push(l2[l1[i]].pseudo);
 		}
 		return ls;
-	}
+	};
 
 	function indice_joueur(l,p){
 		let indice;
@@ -93,7 +94,7 @@ const cartes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
 			}
 		}
 		return coins;
-	}
+	};
 
 module.exports = {
 	carte : carte,
